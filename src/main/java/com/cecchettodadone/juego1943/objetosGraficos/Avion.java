af46a1704bdb6ca1e8ca918c7b048b4f295d3918
Ramiro Cecchetto <ramirocecchetto123@gmail.com>
2023-05-22 11:20:27 -0300

    En el anterior commit no puse la clase Avion.

---
+++ src/main/java/com/cecchettodadone/juego1943/objetosGraficos/Avion.java
@@ -0,0 +1,19 @@
+package com.cecchettodadone.juego1943.objetosGraficos;
+
+import com.cecchettodadone.juego1943.ObjetoGrafico;
+import com.cecchettodadone.juego1943.Util;
+
+public abstract class Avion extends ObjetoGrafico {
+
+    protected int NAVE_DESPLAZAMIENTO_NORMAL;
+    protected int desplazamiento = NAVE_DESPLAZAMIENTO_NORMAL;
+    public void setDesplazamiento(int NAVE_DESPLAZAMIENTO_NORMAL) {
+        this.NAVE_DESPLAZAMIENTO_NORMAL = NAVE_DESPLAZAMIENTO_NORMAL;
+    }
+
+    public Avion (int posX, int posY) {}
+
+    @Override
+    public void update(double delta) {}
+
+}
