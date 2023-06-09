b05e2ea4787db59f55edc74ba652c4b1beec49b3
Ramiro Cecchetto <ramirocecchetto123@gmail.com>
2023-06-09 12:27:14 -0300

    Metodo rotar en objeto grafico

--- src/main/java/com/cecchettodadone/juego1943/ObjetoGrafico.java
+++ src/main/java/com/cecchettodadone/juego1943/ObjetoGrafico.java
@@ -3,6 +3,7 @@ package com.cecchettodadone.juego1943;
 import com.entropyinteractive.Keyboard;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
@@ -52,6 +53,38 @@ public abstract class ObjetoGrafico {
 
     public double getX() {return this.posicion.x;}
     public double getY() {return this.posicion.y;}
+
+    public void rotarImagen(double grados) {
+        // Crear una nueva imagen rotada con el mismo ancho y alto, pero con transparencia
+        BufferedImage imagenRotada = new BufferedImage(imagen.getWidth(), imagen.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        // Obtener el contexto gráfico de la imagen rotada
+        Graphics2D g = imagenRotada.createGraphics();
+
+        // Realizar la rotación alrededor del centro de la imagen
+        AffineTransform at = new AffineTransform();
+        at.setToRotation(Math.toRadians(grados), imagen.getWidth() / 2, imagen.getHeight() / 2);
+        g.drawImage(imagen, at, null);
+        g.dispose();
+
+        // Calcular las dimensiones de la imagen rotada
+        double radianes = Math.toRadians(grados);
+        double seno = Math.abs(Math.sin(radianes));
+        double coseno = Math.abs(Math.cos(radianes));
+        int anchoRotado = (int) Math.round(imagen.getWidth() * coseno + imagen.getHeight() * seno);
+        int altoRotado = (int) Math.round(imagen.getWidth() * seno + imagen.getHeight() * coseno);
+
+        // Crear una nueva imagen con las dimensiones ajustadas y transparencia
+        BufferedImage imagenAjustada = new BufferedImage(anchoRotado, altoRotado, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D gAjustado = imagenAjustada.createGraphics();
+        gAjustado.drawImage(imagenRotada, (anchoRotado - imagenRotada.getWidth()) / 2, (altoRotado - imagenRotada.getHeight()) / 2, null);
+        gAjustado.dispose();
+
+        // Establecer la nueva imagen rotada y ajustada
+        this.imagen = imagenAjustada;
+    }
+
+
 
     public void draw(Graphics2D g) {
         if (this.dimensiones != null) g.drawImage(imagen, (int) posicion.getX(), (int) posicion.getY(), this.dimensiones.width, this.dimensiones.height, null);
